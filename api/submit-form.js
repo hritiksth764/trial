03a82d462d4cf82036a96d93d483346e62d417b8
hritikcore <hritiksethi@Hritiks-MacBook-Pro.local>
2024-08-07 16:06:29 +0530

    seal and signature input fields removed

--- api/submit-form.js
+++ api/submit-form.js
@@ -24,43 +24,12 @@ export default async (req, res) => {
         console.log("Parsed fields:", fields);
         console.log("Parsed files:", files);
 
-        // Access the first element of the array for each file
-        const signatureFile = files.signature ? files.signature[0] : null;
-        const sealFile = files.company_seal ? files.company_seal[0] : null;
-
-        // Check if files are correctly parsed
-        if (!signatureFile || !signatureFile.filepath) {
-          throw new Error("Signature file is missing or invalid");
-        }
-
-        if (!sealFile || !sealFile.filepath) {
-          throw new Error("Company seal file is missing or invalid");
-        }
-
         // Initialize Google Cloud Storage
         const storage = new Storage({
           credentials: JSON.parse(process.env.GOOGLE_CLOUD_STORAGE_CREDENTIALS),
         });
         const bucketName = process.env.GCS_BUCKET_NAME;
         const bucket = storage.bucket(bucketName);
-
-        // Upload Signature
-        const signatureFilename = `signatures/${Date.now()}-${
-          signatureFile.originalFilename
-        }`;
-        await bucket.upload(signatureFile.filepath, {
-          destination: signatureFilename,
-          public: true, // Make the file publicly accessible
-        });
-        const signatureUrl = `https://storage.googleapis.com/${bucketName}/${signatureFilename}`;
-
-        // Upload Company Seal
-        const sealFilename = `seals/${Date.now()}-${sealFile.originalFilename}`;
-        await bucket.upload(sealFile.filepath, {
-          destination: sealFilename,
-          public: true, // Make the file publicly accessible
-        });
-        const sealUrl = `https://storage.googleapis.com/${bucketName}/${sealFilename}`;
 
         // Append data to Google Sheets
         const auth = new google.auth.GoogleAuth({
